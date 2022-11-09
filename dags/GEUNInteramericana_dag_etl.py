4c366f5bbe5f62738a3e3850bcab34dca0be282d
Alfredo Ignacio Rodriguez <71676833+elalfredoignacio@users.noreply.github.com>
2022-11-09 16:38:57 -0300

    Nueva version de los DAG con E+T
    Se agreda la etapa de transformacion a los DAGs

--- dags/GEUNInteramericana_dag_etl.py
+++ dags/GEUNInteramericana_dag_etl.py
@@ -6,6 +6,7 @@ from airflow.operators.python import PythonOperator
 #from operators.s3_to_postgres_operator import S3ToPostgresOperator
 #from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 import pandas as pd
+from dateutil.relativedelta import relativedelta
 
 #defino un diccionario con las variables del DAG
 default_args = {
@@ -16,10 +17,6 @@ default_args = {
     'owner' : 'alfredo'
     }
 
-#path_sql = '/usr/local/airflow/include/GE_UniInteramericana.sql',
-#path_csv = '/usr/local/airflow/tests/GE_LaPampa_select.csv'
-# se pueden definir también con op_args = [arg1, arg2]
-
 # defino la funcion de extraccion y generacion de los csv
 def extract_csv():
     with open (f'/usr/local/airflow/include/GE_UniInteramericana.sql', 'r') as sqfile:
@@ -29,12 +26,92 @@ def extract_csv():
     df = hook.get_pandas_df(sql=query)
     df.to_csv('/usr/local/airflow/tests/GE_Interamericana_select.csv')
 
+# defino la funcion de transformación
+def transform_pandas():
+    #se cargan los datasets
+    df_cp = pd.read_csv('/usr/local/airflow/tests/codigos_postales.csv')
+    df = pd.read_csv('/usr/local/airflow/tests/GE_Interamericana_select.csv')
+
+    #renombro la columna 'Unnamed: 0' por 'Id'
+    df = df.rename(columns = {'Unnamed: 0': 'Id'})
+
+    df['university'] = df['university'].astype(str)
+    df['university'] = df['university'].str.lower().str.replace("^-", "").str.replace("-", " ").str.replace("  ", " ")
+
+    df['career'] = df['career'].astype(str)
+    df['career'] = df['career'].str.lower().str.replace("^-", "").str.replace("-", " ").str.replace("  ", " ")
+
+    df['last_name'] = df['last_name'].str.lower()
+    new = df['last_name'].str.split("-", n = 1, expand = True)
+    df['first_name']= new[0]
+    df['last_name']= new[1]
+
+    df['gender'].iloc[df['gender'] == 'M'] = 'male'
+    df['gender'].iloc[df['gender'] == 'F'] = 'female'
+
+    # fecha de nacimiento en formato YY(dos digitos)/mes(string)/dd
+    df['fecha_nacimiento'] = pd.to_datetime(df['fecha_nacimiento'], format='%y/%b/%d')
+    df['inscription_date'] = pd.to_datetime(df['inscription_date'], format='%Y-%m-%d')
+    
+    # a todas las fechas que son mayores a 2022, le resto 100 años
+    k = len(df.axes[0])
+    for i in range(k):
+        if df['fecha_nacimiento'][i].strftime('%Y') >= '2022':
+            df['fecha_nacimiento'][i] = df['fecha_nacimiento'][i] - relativedelta(years=100)
+
+    # elimino las filas con fecha de nacimiento mayor a la fecha de inscripcion y reseteo indices
+    df = df.drop(df[df['inscription_date'] <= df['fecha_nacimiento']].index)
+    df = df.reset_index(drop=True)
+
+    # vuelvo a pasar el campo 'inscription_date' a string
+    df['inscription_date'] = df['inscription_date'].astype(str)
+
+    df['age'] = datetime.now() - df['fecha_nacimiento']
+    df['age'] = (df['age'].dt.days)/365
+    df['age'] = df['age'].astype(int)
+
+    # elimino los datos con edades menores a 18
+    df = df.drop(df[df['age'] < 18].index)
+    df = df.reset_index(drop=True)
+    # elimino la columna fecha de nacimiento
+    df = df.drop(['fecha_nacimiento'], axis=1)
+    
+    df['location'] = df['location'].astype(str)
+    df['location'] = df['location'].str.lower().str.replace("-", " ").str.replace("  ", " ")
+
+    df_cp['localidad'] = df_cp['localidad'].astype(str)
+    df_cp['localidad'] = df_cp['localidad'].str.lower().str.replace("-", " ").str.replace("  ", " ")
+
+    # como existen localidades duplicadas con diferente CP, elimino las duplicadas 
+    # y me quedo con la ultima suponiendo que es el registro mas nuevo
+    df_cp = df_cp.drop_duplicates(['localidad'], keep='last')
+
+    # inner join para obtener el codigo posatal partir de la localidad
+    inner_df = pd.merge(left=df,right=df_cp, left_on='location', right_on='localidad')
+
+    # elimino las columnas 'location' y 'codigo_postal'
+    inner_df = inner_df.drop(['postal_code', 'localidad'], axis=1)
+    #renombro columna codigo postal
+    inner_df = inner_df.rename(columns = {'codigo_postal': 'postal_code'})
+    df = inner_df
+    #paso el CP a string
+    df['postal_code'] = df['postal_code'].astype(str)
+
+    # paso email a string (ya está pero por las dudas)
+    # lo llevo todo a minusculas, elimino espacios extras y elimino guiones
+    df['email'] = df['email'].astype(str)
+    df['email'] = df['email'].str.lower().str.replace("-", " ").str.replace(" ", "")
+
+    # Pasaje de dataframe a archivo de texto
+    df.to_csv('/usr/local/airflow/tests/GE_Interamericana_process.txt', sep='\t', index=False)
+
+
 # se define el DAG
 with DAG(
-    dag_id = "DAG_Uni_Interamericana",
+    dag_id = "DAG_Uni_Interamericana_ET",
     default_args = default_args,
     schedule_interval="@hourly",
-    tags = ['ETL Universidad Interamericana']
+    tags = ['ETL Universidad Interamericana (extract&transform)']
 ) as dag:
 
 # se definen los tasks
@@ -45,13 +122,14 @@ with DAG(
     
     )
 
-    #tarea_2 = PythonOperator(
-    #    task_id = "transform",
-    #)
+    tarea_2 = PythonOperator(
+        task_id = "transform",
+        python_callable = transform_pandas
+    )
 
     #tarea_3 = S3ToPostgresOperator(
     #    task_id = "load",
     #)
 
 # se definen las dependencias de las tareas
-    tarea_1 # >> tarea_2 >> tarea_3
+    tarea_1 >> tarea_2 #>> tarea_3
--- dags/GEUNLaPampa_dag_etl.py
+++ dags/GEUNLaPampa_dag_etl.py
@@ -6,6 +6,8 @@ from airflow.operators.python import PythonOperator
 #from operators.s3_to_postgres_operator import S3ToPostgresOperator
 #from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 import pandas as pd
+#from plugins.data_transform import extract_csv
+
 
 #defino un diccionario con las variables del DAG
 default_args = {
@@ -29,12 +31,79 @@ def extract_csv():
     df = hook.get_pandas_df(sql=query)
     df.to_csv('/usr/local/airflow/tests/GE_LaPampa_select.csv')
 
+# defino la funcion de transformación
+def transform_pandas():
+    # se cargan los datasetes
+    df_cp = pd.read_csv('/usr/local/airflow/tests/codigos_postales.csv')
+    df = pd.read_csv('/usr/local/airflow/tests/GE_LaPampa_select.csv')
+    #renombro la columna 'Unnamed: 0' por 'Id'
+    df = df.rename(columns = {'Unnamed: 0': 'Id'})
+
+    df['university'] = df['university'].astype(str)
+    df['university'] = df['university'].str.lower().str.replace("^-", "").str.replace("-", " ").str.replace("  ", " ")
+
+    df['career'] = df['career'].astype(str)
+    df['career'] = df['career'].str.lower().str.replace("^-", "").str.replace("-", " ").str.replace("  ", " ")
+
+    df['last_name'] = df['last_name'].str.lower()
+    # separo nombre de apellido (todo guardado en last_name)
+    new = df['last_name'].str.split(" ", n = 1, expand = True)
+    df['first_name']= new[0]
+    df['last_name']= new[1]
+
+    df['gender'].iloc[df['gender'] == 'M'] = 'male'
+    df['gender'].iloc[df['gender'] == 'F'] = 'female'
+
+    # convierto las columnas en formato fecha
+    df['fecha_nacimiento'] = pd.to_datetime(df['fecha_nacimiento'], format='%d/%m/%Y')
+    df['inscription_date'] = pd.to_datetime(df['inscription_date'], format='%Y-%m-%d')
+    # elimino las filas con fecha de nacimiento mayor a la fecha de inscripcion y reseteo indices
+    df = df.drop(df[df['inscription_date'] <= df['fecha_nacimiento']].index)
+    df = df.reset_index(drop=True)
+ 
+    # vuelvo a convertir a 'inscription_date' en string
+    df['inscription_date'] = df['inscription_date'].astype(str)
+
+    # Cálculo de la edad
+    df['age'] = datetime.now() - df['fecha_nacimiento']
+    df['age'] = (df['age'].dt.days)/365
+    df['age'] = df['age'].astype(int)
+    # elimino los datos con edades menores a 18 y reseteo índice 
+    df = df.drop(df[df['age'] < 18].index)
+    df = df.reset_index(drop=True)
+    # elimino la columna fecha de nacimiento
+    df = df.drop(['fecha_nacimiento'], axis=1)
+
+    # inner join para obtener la localidad a partir del código postal
+    inner_df = pd.merge(left=df,right=df_cp, left_on='postal_code', right_on='codigo_postal')
+    # elimino las columnas 'location' y 'codigo_postal'
+    inner_df = inner_df.drop(['location', 'codigo_postal'], axis=1)
+    #renombro columna localidad
+    inner_df = inner_df.rename(columns = {'localidad': 'location'})
+    df = inner_df
+    # paso location a string (ya está, pero por las dudas)
+    # lo llevo todo a minusculas, elimino espacios extras y elimino guiones
+    df['location'] = df['location'].astype(str)
+    df['location'] = df['location'].str.lower().str.replace("-", " ").str.replace("  ", " ")
+
+    # paso email a string (ya está, pero por las dudas)
+    # lo llevo todo a minusculas, elimino espacios extras y elimino guiones
+    df['email'] = df['email'].astype(str)
+    df['email'] = df['email'].str.lower().str.replace("-", " ").str.replace(" ", "")
+
+    # llevo el campo postal_code a string
+    df['postal_code'] = df['postal_code'].astype(str)
+
+    #Pasaje de dataframe a archivo de texto
+    df.to_csv('/usr/local/airflow/tests/GE_LaPampa_process.txt', sep='\t', index=False)
+
+
 # se define el DAG
 with DAG(
-    dag_id = "DAG_Uni_LaPampa",
+    dag_id = "DAG_Uni_LaPampa_ET",
     default_args = default_args,
     schedule_interval="@hourly",
-    tags = ['ETL Universidad La Pampa']
+    tags = ['ETL Universidad La Pampa (extract&transform)']
 ) as dag:
 
 # se definen los tasks
@@ -45,13 +114,14 @@ with DAG(
     
     )
 
-    #tarea_2 = PythonOperator(
-    #    task_id = "transform",
-    #)
+    tarea_2 = PythonOperator(
+        task_id = "transform",
+        python_callable = transform_pandas
+    )
 
     #tarea_3 = S3ToPostgresOperator(
     #    task_id = "load",
     #)
 
 # se definen las dependencias de las tareas
-    tarea_1 # >> tarea_2 >> tarea_3
+    tarea_1 >> tarea_2 #>> tarea_3
